4644fa34f03069a211581e9993489d20a8aa977f
shubham <shubhamzirpe304.com>
2026-04-21 15:06:44 +0530

    demo

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,7 @@ import adminRouter from './routes/adminRoute.js'
 import doctorRouter from './routes/doctorRoute.js'
 import userRouter from './routes/userRoute.js'
 
+
 // app config
 const app = express()
 const port = process.env.PORT || 4000
